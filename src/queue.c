c8ff6b8d95c67c67cb88f0359ad434712166985f
Sion Chaudhuri <sion.chaudhuri@gmail.com>
2013-04-19 04:34:33 -0700

    Fixed license block for queues

--- src/queue.c
+++ src/queue.c
@@ -5,23 +5,16 @@
  * 
  * by: Steven Skiena
  * begun: March 27, 2002
- * 
  *
  * Copyright 2003 by Steven S. Skiena; all rights reserved. 
  * 
  * Permission is granted for use in non-commerical applications
  * provided this copyright notice remains intact and unchanged.
  * 
- * This program appears in my book:
- * 
- " Programming Challenges: The Programming Contest Training Manual"
- * by Steven Skiena and Miguel Revilla, Springer-Verlag, New York 2003.
- * 
- * See our website www.programming-challenges.com for additional information.
- * 
- * This book can be ordered from Amazon.com at
- * 
- * http://www.amazon.com/exec/obidos/ASIN/0387001638/thealgorithmrepo/
+ * modified for the Guacamole project by:
+ *   Craig Hokanson <craig.hokanson@sv.cmu.edu>
+ *   Sion Chaudhuri <sion.chaudhuri@west.cmu.edu>
+ *   Gio Perez <gio.perez@sv.cmu.edu>
  * 
  * ***** END LICENSE BLOCK ***** */
  
@@ -44,7 +37,6 @@ int enqueue(queue* q, void* data, int size) {
         return -1;
         
     q->last = (q->last+1) % QUEUESIZE;
-    // q->items[ q->last ] = data; 
     memcpy(q->items[ q->last ], data, size);   
     q->count = q->count + 1;
     
@@ -55,7 +47,6 @@ int dequeue(queue* q, void* data, int size) {
     if (q->count <= 0) 
         return -1;
         
-    // data = q->items[ q->first ];
     memcpy(data, q->items[ q->first ], size);  
     q->first = (q->first+1) % QUEUESIZE;
     q->count = q->count - 1;
